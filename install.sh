d6e3e504d63d1f72ca1ac735f5527e8e1e9c4523
maxobur0001 <msblock09@gmail.com>
2024-12-25 01:05:55 +0300

    Polybar added, install script added, i3blocks deleted, commented some configs

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install pacman packages
+pacman -Sy \
+    i3-wm \
+    rofi \
+    neovim \
+    picom \
+    polybar \
+    kitty \
+    zsh \
+    yazi \
+    fastfetch \
+    dunst \
+    obsidian \
+    telegram-desktop \
+    spotify-launcher \
+
